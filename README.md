5e55978c96d6ab964600c7ac2ba96f69ff619897
Your Name <cacabruno689@gmail.com>
2022-04-13 19:43:21 -0300

    Alterei o ReadMe Coloquei o comando YARN e NPM

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# How-To-Make
+## How-To-Make
 Olá.Esqueceu como Centralizar uma div, imagem ou até texto? Agorá com uma CLI que te ajuda.
 Feita em NodeJS ela irá te ajudar em qualquer coisa
-# Setup
+## Comandos
+Para saber os comandos difite  no seu terminal <code>how tutorial</code>
+Ou veja no repositorio a **lista.json**
 
+## Setup
--- index.js
+++ index.js
@@ -10,12 +10,12 @@ program
     .command('tutorial')
     .description('Adiciona um to-do')
     .action(() => {
-       return console.log(Lista.nomes.lista)
+        return console.log(Lista.nomes.lista)
     });
-    program
+program
     .command("center")
     .description(Lista.nomes.lista.center)
-    .action(()=>{
+    .action(() => {
         return console.log(chalk.red(`
         display:flex;
         align-items:center;
@@ -23,12 +23,51 @@ program
         Gostaria de agradeçer a DevMedia Por ter Disponibilizado
         `))
     })
-    program
+program
     .command("node")
     .description(Lista.nomes.lista.node)
     .action(() => {
         return console.log(chalk.green(`
-        Runtime For JavaScript run at V8 Engine 
+        Runtime For JavaScript run at V8 Engine (Not run TypeScript Native)
         `))
     })
+program
+    .command("NPM")
+    .description(Lista.nomes.lista.NPM)
+    .action(() => {
+        console.log(chalk.blue(` 
+ Npm is the world's largest software registry. Open source developers from every continent use npm to share and borrow packages, and many organizations use npm to manage private development as well.
+
+npm consists of three distinct components:
+
+the website
+the Command Line Interface (CLI)
+the registry
+Use the website to discover packages, set up profiles, and manage other aspects of your npm experience. For example, you can set up organizations to manage access to public or private packages.
+
+The CLI runs from a terminal, and is how most developers interact with npm.
+
+The registry is a large public database of JavaScript software and the meta-information surrounding it.
+        `))
+        console.log(chalk.red(`
+        Commands: npm install <your package> 
+          npm link 
+          npm init
+          npm init -y (run npm init automaticy)
+          npm version
+          `))
+    })
+program
+    .command("yarn")
+    .description(Lista.nomes.lista.Yarn)
+    .action(() => {
+        console.log(chalk.blue(`
+        Yarn is a package manager for your code. It allows you to use and share code with other developers from around the world. Yarn does this quickly, securely, and reliably so you don't ever have to worry.
+
+        Yarn allows you to use other developers' solutions to different problems, making it easier for you to develop your software. If you have problems, you can report issues or contribute back on GitHub, and when the problem is fixed, you can use Yarn to keep it all up to date.
+
+        Code is shared through something called a package. A package contains all the code being shared as well as a package.json file (called a manifest) which describes the package.
+        `))
+    })
+    
 program.parse(process.argv);
